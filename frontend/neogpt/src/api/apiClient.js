083b7d98c2b1d26ac0a57d5fb6f2769bd9975c85
Mr. Rahul Chowdhury <96120918+Rahul-2314@users.noreply.github.com>
2025-11-05 01:13:35 +0530

    Refactor apiClient to use dynamic baseURL
    Updated the baseURL to use an environment variable with a fallback.

--- frontend/neogpt/src/api/apiClient.js
+++ frontend/neogpt/src/api/apiClient.js
@@ -3,12 +3,16 @@ import axios from "axios";
 const apiClient = axios.create({
 	// baseURL: "http://localhost:5000",
 	// baseURL: "https://neogpt-1.onrender.com",
-	baseURL: "https://neogpt-backend.vercel.app",
+	// baseURL: "https://neogpt-backend.vercel.app",
+	baseURL: `${
+		import.meta.env.VITE_API_BASE_URL || "https://neogpt-backend.vercel.app"
+	}`,
 	headers: {
 		"Content-Type": "application/json",
 	},
 });
 
 export default apiClient;
+
 
 
