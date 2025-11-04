a69555fb895f3402db500afdae796a8ade7361ec
Mr. Rahul Chowdhury <96120918+Rahul-2314@users.noreply.github.com>
2025-11-04 23:52:00 +0530

    Update API base URL in apiClient.js

--- frontend/neogpt/src/api/apiClient.js
+++ frontend/neogpt/src/api/apiClient.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 const apiClient = axios.create({
 	// baseURL: "http://localhost:5000",
-	baseURL: "https://neogpt-3jcf.onrender.com",
+	baseURL: "https://neogpt-1.onrender.com",
 	headers: {
 		"Content-Type": "application/json",
 	},
 });
 
 export default apiClient;
+
